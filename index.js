f9030a0bcab14a1c1fbfc961b1aae926a76c4ad3
abdullah <ff245185@gmail.com>
2022-11-03 15:34:03 +0600

    last update

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
  app.use(cors());
 
 const Catagroy = require('./data/Catagroy.json')
+const news = require('./data/news.json')
 
 app.get('/',(req, res)=>{
     res.send('abdullah ')
@@ -15,7 +16,25 @@ app.get('/',(req, res)=>{
 
 app.get('/news-Catagroy',(req, res)=>{
     res.send(Catagroy)
+});
+app.get('/news',(req, res)=>{
+    res.send(news)
+});
+
+app.get('/news-catagroy/:id', (req,res)=>{
+    const id = req.params.id;
+        const catagroy_Catagroy =  Catagroy.filter(n => n.id == id);
+        res.send(catagroy_Catagroy)
+    
+      
+  
 })
+
+app.get('/news/:id', (req, res)=>{
+  const id = req.params.id;
+  const selectNews =  news.find( n => n._id === id);
+  res.send(selectNews)
+});
 
 app.listen(port,()=>{
     console.log('hi brother no one help',port);
